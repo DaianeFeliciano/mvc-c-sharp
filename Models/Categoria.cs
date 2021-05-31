c2f65a0efe302aadc29326fb0570c7f2c600a40c
Daiane Feliciano <77757301+DaianeFeliciano@users.noreply.github.com>
2021-05-30 23:19:33 -0300

    'parte 1 mvc'

---
+++ Models/Categoria.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace CursoMVC.Models
+{
+    public class Categoria
+    {
+        public int Id { get; set;  }// adcc propriedades na classe, todas as clases vai ter um id inteiro que será a chave primária da
+                                    // nossa tabela no banco de dados # modelo code first do frameork
+        public string Descricao { get; set; }
+    }
+}
---
+++ Models/Context.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CursoMVC.Models
+{
+    public class Context : DbContext // classe herda do entity framework dbcontext herda dessa interface
+    {
+        public DbSet<Categoria> Categorias { get; set; } // ta falando que tenho uma tabela chamada categoria quando executar
+                                                         // esse context vai criar e no momento que eu precisar acessar basta referenciar esse context
+            // fala para entiy que vai ter uma tabela de categoria informar qual tipo da classe e precisa deixar no startup ele visivel
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)// tem metodo dentro dele q vai subscrever metodo responsavel por configurar o entity frameork
+
+        {
+            optionsBuilder.UseSqlServer(connectionString:@"Server=(localdb)\mssqllocaldb;Database=Cursomvc;Integrated Security=True"); // string de conexão 
+        }
+    }
+}
