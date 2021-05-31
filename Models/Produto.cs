9149fe363e14c5b5439eb6ea497ef515e41af5cd
Daiane Feliciano <77757301+DaianeFeliciano@users.noreply.github.com>
2021-05-31 13:39:58 -0300

    'Adicionando classe produto - models e controllers'
    MVC - C#

---
+++ Models/Produto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CursoMVC.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; } //atalho prop cria as propriedades mais rápidas
+        public string Descricao { get; set; }
+        public int Quantidade { get; set; }
+
+        public int CategoriaId { get; set; }
+
+        public Categoria Categoria { get; set; }
+
+
+    }
+}
